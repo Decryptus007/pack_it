e41be146636f902df456ad9ccc096026049edea5
Decryptus007 <domnick080@outlook.com>
2022-04-03 01:34:23 +0100

    skip chat comp, reqDelivery next

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 import Home from './components/Layout/Home/Home'
 import Wallet from './components/Layout/Wallet/Wallet'
 import Notification from './components/Layout/Notification/Notification'
+import Support from "./components/Layout/Support/Support"
 import Settings from "./components/Layout/Settings/Settings"
 import EditProfile from "./components/Layout/Settings/EditProfile/EditProfile"
 import AcctDetail from "./components/Layout/Settings/AcctDetail/AcctDetail"
@@ -65,6 +66,11 @@ function App() {
         showSideBarState={showSideBar}
       />} />
       <Route path="notification" element={<Notification
+        signOutUser={signOutUser}
+        showSideBar={showSideBarFunc}
+        showSideBarState={showSideBar}
+      />} />
+      <Route path="support" element={<Support
         signOutUser={signOutUser}
         showSideBar={showSideBarFunc}
         showSideBarState={showSideBar}
