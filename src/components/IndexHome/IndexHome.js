0e4c4d3954db8367fe2fcd6602ba845101f7e0f1
decryptus007 <domnick080@outlook.com>
2022-10-17 17:13:37 +0100

    fix index page

--- src/components/IndexHome/IndexHome.js
+++ src/components/IndexHome/IndexHome.js
@@ -3,16 +3,20 @@ import IndexHeader from './IndexHeader'
 import IndexSevices from './IndexSevices'
 import IndexFeatures from './IndexFeatures'
 import IndexTestimonials from './IndexTestimonials'
+import IndexAbout from './IndexAbout'
+import IndexFooter from './IndexFooter'
 
 const IndexHome = () => {
   return (
-    <main className="container mx-auto">
+    <main className="container bg-white mx-auto">
       <IndexHeader />
       <section class="mt-12 md:mt-24">
         <IndexSevices />
         <IndexFeatures />
         <IndexTestimonials />
+        <IndexAbout />
       </section>
+      <IndexFooter />
     </main>
   )
 }
